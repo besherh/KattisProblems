83ca52fb6eec379ee3a54ab8d7b3f1b68fb80ca0
Besher Alhalabi <b.alhalabi@gmail.com>
2017-07-03 15:34:27 +0200

    Create Sol.cs

---
+++ Ladder/CSharp/Sol.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Ladder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var readLine = Console.ReadLine();
+                if (readLine == null) return;
+                var input = readLine.Split(new char[0]);
+                var h = int.Parse(input[0]);
+                var v = int.Parse(input[1]);
+                if ((h >= 1 && h <= 10000) && (v >= 1 && v <= 89))
+                {
+                    var result = Math.Ceiling(CalculateMinLength(h, v));
+                    Console.WriteLine(result);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public static double CalculateMinLength(int h, int v)
+        {
+            return (h/Math.Sin(ConvertToRadians(v)));
+        }
+
+        public static double ConvertToRadians(double a)
+        {
+            return (Math.PI/180)*a;
+        }
+    }
+}
